da3e0bd0821769ba94df228f3b9a18f58564ab3a
Lincoln Yu <lincoln.yu@xpsolutions.com>
2015-11-26 17:02:36 +1000

    added initial files

---
+++ TeighaScourer/TeighaScourer/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace TeighaScourer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dir;
+            if (args.Length > 0)
+            {
+                dir = args[0];
+            }
+            else
+            {
+                dir = Directory.GetCurrentDirectory();
+            }
+            Scour(dir);
+        }
+
+        private static void Scour(string dir)
+        {
+            var dirInfo = new DirectoryInfo(dir);
+            var fns = new HashSet<string>();
+            foreach (var f in dirInfo.GetFiles())
+            {
+                var ext = f.Extension.ToLower();
+                if (ext == ".dll" || ext == ".tx" || ext == ".txv")
+                {
+                    var fn = Path.GetFileNameWithoutExtension(f.Name.ToLower());
+                    fns.Add(fn);
+                }
+            }
+            foreach (var f in dirInfo.GetFiles())
+            {
+                var ext = f.Extension.ToLower();
+                if (ext == ".dll" || ext == ".tx" || ext == ".txv")
+                {
+                    continue;
+                }
+                if (ext == ".pdb")
+                {
+                    var fn = Path.GetFileNameWithoutExtension(f.Name.ToLower());
+                    if (fns.Contains(fn))
+                    {
+                        continue;
+                    }
+                }
+                f.Delete();
+            }
+        }
+    }
+}
